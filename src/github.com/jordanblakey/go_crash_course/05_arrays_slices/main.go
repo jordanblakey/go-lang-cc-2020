04567d8f31d201d16779b63922624f3bdf67dc41
Jordan Blakey <454820+jordanblakey@users.noreply.github.com>
2020-11-18 09:25:32 -0600

    Completed packages, functions, and arrays lessions

---
+++ src/github.com/jordanblakey/go_crash_course/05_arrays_slices/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arrays
+	var fruitArray [2]string
+
+	// Assign values
+	fruitArray[0] = "Apple"
+	fruitArray[1] = "Orange"
+
+	fmt.Println(fruitArray)
+	fmt.Println(fruitArray[0])
+	fmt.Println(fruitArray[1])
+}
